c8f47d9730d520e6d95919f56850554c1461ce1f
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2024-03-18 16:14:52 +0330

    Update A2_finonacci.py

--- A2/A2_finonacci.py
+++ A2/A2_finonacci.py
@@ -2,6 +2,10 @@ x = int(input('Enter a number'))
 a = 0
 b = 1
 
+while x == 0:
+    print('invalid number')
+    x = int(input('Enter a number'))
+
 for i in range(1, x+1):
     print(a, end = ",")
     c = a+b
